fc44f56ed5fc9ed378ed1b4bd645a25bd2d21ea8
JaryMontoya <jarycarolinam125@gmail.com>
2024-06-02 04:43:18 -0500

    commit

--- capicua-detector/src/app/app.module.ts
+++ capicua-detector/src/app/app.module.ts
@@ -2,7 +2,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';  // Añadir esta línea
-
 import { AppComponent } from './app.component';
 import { CapicuaDetectorComponent } from './capicua-detector/capicua-detector.component';
 
